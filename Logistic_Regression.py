05265b5b9178e9cf3f36ab2e106a37d91915e3b2
Parakh24 <parakhvirnawe2404@gmail.com>
2025-09-11 22:15:19 +0530

    commenting the codes for clear explanation

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -7,6 +7,23 @@ import matplotlib.pyplot as plt
 
 class LogisticRegressionScratch:
     def __init__(self , learning_rate = 0.01 , n_iter = 1000 , fit_intercept = True , verbose = False):
+
+        """
+        Parameters
+        ----------
+        self : self refers to the current instance of the class, without self, variables inside __init__ would disappear once it ends 
+        learning_rate : float, optional
+            Step size parameter, defaults to 0.01
+        n_iter : int, optional
+            Number of iterations, defaults to 1000
+        fit_intercept : bool, optional
+            Whether the intercept should be estimated or not. If set to
+            False, no intercept will be used in calculations (e.g. data is
+            already centered). Defaults to True
+        verbose : bool, optional
+            Controls the verbosity of the object, defaults to False
+        """
+
         self.lr = learning_rate
         self.epoch = n_iter 
         self.fit_intercept = fit_intercept
@@ -14,16 +31,63 @@ class LogisticRegressionScratch:
 
 
     def _add_intercept(self , X):
+
+        """
+        If fit_intercept is True, this adds a column of ones to the
+        beginning of the design matrix X. This is used to represent the
+        intercept term in the linear model.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data
+
+        Returns
+        -------
+        X : array-like of shape (n_samples, n_features + 1)
+            Design matrix with intercept term
+        """
+
         if self.fit_intercept:
             return np.hstack([np.ones((X.shape[0] , 1)), X]) 
         return X 
     
 
     def _sigmoid(self , z):
+
+        """
+        The sigmoid function maps real numbers to the interval [0,1].
+
+        Parameters
+        ----------
+        z : array-like of shape (n_samples, n_features)
+            Input array
+
+        Returns
+        -------
+        y : array-like of shape (n_samples, n_features)
+            Output array
+        """
+
         return 1 / (1+np.exp(-z)) 
     
 
     def fit(self,X,y):
+
+
+        """
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Training data
+        y : array-like of shape (n_samples,)
+            Target values
+        Returns
+        -------
+        self : object
+            Returns the instance of the class
+        """
+
 
         X = self._add_intercept(X) 
         m,n = X.shape 
@@ -35,7 +99,7 @@ class LogisticRegressionScratch:
             grad = (1/m) * X.T.dot(y_pred-y) 
             self.w -= self.lr * grad 
 
-
+            
             if self.ver and i%(self.epoch // 10 or 1) == 0: 
                loss = -(1/m) * np.sum(y*np.log(y_pred+1e-9) + (1-y)*np.log(1-y_pred+1e-9))
                print(f"Iter {i:4d} | Loss: {loss:.8f}") 
@@ -43,6 +107,22 @@ class LogisticRegressionScratch:
         return self 
     
     def predict(self, X): 
+
+        """
+        Predict using the logistic model.
+
+        Parameters
+        ----------
+        X : array-like of shape (n_samples, n_features)
+            Samples.
+
+        Returns
+        -------
+        y_pred : array of shape (n_samples,)
+            Returns predicted values.
+        """
+
+
         X = self._add_intercept(X) 
         return np.where(self._sigmoid(X.dot(self.w)) >= 0.5 , 1 , 0)
     
