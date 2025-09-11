889896c96a6f6ff4b18f3705523ac0af5fe6a3f5
Parakh24 <parakhvirnawe2404@gmail.com>
2025-09-11 16:12:47 +0530

    checking onto the mathematical version of updated weights

--- Linear_Regression.py
+++ Linear_Regression.py
@@ -25,15 +25,15 @@ class LinearRegressionScratch:
               Controls the verbosity of the object, defaults to False
           """
           
-    
+        
           self.lr = learning_rate  
           self.epoch = n_iter 
           self.fit_incpt = fit_intercept 
           self.ver = verbose 
-     
+            
      #_add_intercept is a convention that it is a private file and users outside the class are not allowed to acces this function
      def _add_intercept(self,X):
-          
+              
           """ 
           If fit_intercept is True, this adds a column of ones to the 
           beginning of the design matrix X. This is used to represent the 
@@ -46,7 +46,7 @@ class LinearRegressionScratch:
           
           return X  #if bias does not exist , we return feature vector itself 
      
-     def fit(self , X , y):
+     def fit(self , X , y):  
 
         """
         Parameters
@@ -57,28 +57,28 @@ class LinearRegressionScratch:
             Target values
 
         Returns 
-        -------
+        ----------
         self : object
             Returns the instance of the class
         """
     
 
-        X = self._add_intercept(X) 
-        m,n = X.shape
-        self.w = np.zeros(n)  
+        X = self._add_intercept(X)        #calling the addintercept function
+        m,n = X.shape    
+        self.w = np.zeros(n)              #weights are defined in terms of instance so that it could be accessed by other functions calls
+             
+        for i in range(self.epoch):       #no of iterations to set the correct parameters for weights using batch gradient descent 
+             y_pred = X.dot(self.w)       #predicted value of y by the model 
+             error = y_pred - y           #error found  
+             grad = (2/m)*X.T.dot(error)  #gradient descent
+             self.w -= self.lr*grad       #updated weight  
 
-        for i in range(self.n_iter):
-             y_pred = X.dot(self.w)
-             error = y_pred - y
-             grad = (2/m)*X.T.dot(error) 
-             self.w -= self.lr*grad 
-
-             if self.verbose and i%(self.n_iter//10 or 1) == 0:
+             if self.verbose and i%(self.n_iter//10 or 1) == 0: 
                   loss = mean_squared_error(y,y_pred)
                   print(f"Iter {i:4d} | Loss: {loss:.4f}")
 
         return self 
-     
+                 
      def predict(self, X):
 
           """
@@ -99,13 +99,22 @@ class LinearRegressionScratch:
           return X.dot(self.w) 
      
 
-X, y = make_regression(n_samples=200, n_features=3, noise=10, random_state=42) 
-X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42) 
-scaler = StandardScaler() 
-X_train = scaler.fit_transform(X_train) 
+X, y = make_regression(n_samples=200, n_features=3, noise=10, random_state=42)
+
+X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
+
+scaler = StandardScaler()
+
+X_train = scaler.fit_transform(X_train)
+
 X_test = scaler.transform(X_test)
-model = LinearRegressionScratch(lr=0.05, n_iter=1000, verbose=True) 
+
+model = LinearRegressionScratch(lr=0.05, n_iter=1000, verbose=True)
+
 model.fit(X_train, y_train) 
-y_pred = model.predict(X_test) 
+
+y_pred = model.predict(X_test)
+
 print("MSE:", mean_squared_error(y_test, y_pred)) 
-print("R²:", r2_score(y_test, y_pred))
+
+print("R²:", r2_score(y_test, y_pred)) 
