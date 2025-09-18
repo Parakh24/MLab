09df579cd4c6ecf37756695c30a0518e9e2fbf5b
Parakh24 <parakhvirnawe2404@gmail.com>
2025-09-18 11:50:34 +0530

    new libraries have been added

--- KNN_2.py
+++ KNN_2.py
@@ -1,5 +1,5 @@
 import pandas as pd
-import numpy as np 
+
 from sklearn.neighbors import KNeighborsClassifier 
 from sklearn.metrics import mean_squared_error,mean_absolute_error 
 from sklearn.model_selection import train_test_split 
