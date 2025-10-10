b50d3351948a44afe17cc96835d18e65e0557d6c
Parakh24 <parakhvirnawe2404@gmail.com>
2025-10-10 20:55:39 +0530

    another method for cleansing data

---
+++ Normalization_2.py
@@ -0,0 +1,8 @@
+#onehotEncoder 
+from sklearn.preprocessing import OneHotEncoder 
+import numpy as np 
+
+data = np.array([['Red'] , ['Blue'] , ['Green'] , ['Purple']]) 
+one = OneHotEncoder(sparse_output= False) 
+encoded = one.fit_transform(data)
+print(encoded)  
