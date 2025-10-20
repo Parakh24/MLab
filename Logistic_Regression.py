5087de0973a636664cbcbb28ed39f23591161c32
Parakh24 <parakhvirnawe2404@gmail.com>
2025-10-20 22:28:46 +0530

    swapped X_test with y_train

--- Logistic_Regression.py
+++ Logistic_Regression.py
@@ -17,7 +17,7 @@ df = pd.DataFrame(data , index = [1 , 2 , 3 , 4 , 5 , 6])
 X = df[["Study_Hours"]]   #independent variable 
 y = df["Pass_Fail"]       #dependent variable 
 
-X_train,y_train,X_test,y_test = train_test_split(X,y,test_size=0.2,random_state=42) 
+X_train,X_test,y_train,y_test = train_test_split(X,y,test_size=0.2,random_state=42) 
 
 #model
 model = LogisticRegression() 
