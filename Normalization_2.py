d6c48072afcd1cf08e77d1ea771a4c5d5fa77293
Parakh24 <parakhvirnawe2404@gmail.com>
2025-10-25 22:04:08 +0530

    no new lines of code

--- Normalization_2.py
+++ Normalization_2.py
@@ -5,4 +5,5 @@ import numpy as np
 data = np.array([['Red'] , ['Blue'] , ['Green'] , ['Purple']]) 
 one = OneHotEncoder(sparse_output= False) 
 encoded = one.fit_transform(data)
-print(encoded)  
+print(encoded) 
+
