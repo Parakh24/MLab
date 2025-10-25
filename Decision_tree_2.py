d3e999f2bf9420aa05d9aa69618c5ea60121d45e
Parakh24 <parakhvirnawe2404@gmail.com>
2025-10-25 21:56:35 +0530

    no new lines of code

--- Decision_tree_2.py
+++ Decision_tree_2.py
@@ -22,7 +22,7 @@ X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.3, random_
 
 # Decision Tree Regressor
 reg = DecisionTreeRegressor(criterion='squared_error', max_depth=3, random_state=42)
-reg.fit(X_train, y_train)
+reg.fit(X_train, y_train) 
 
 # Predictions
 predictions = reg.predict(X_test)
